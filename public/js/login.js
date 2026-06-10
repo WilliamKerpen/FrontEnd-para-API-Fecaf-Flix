fd0e398ed7b3622d7f15fb9e97624af3160686dd
WilliamKerpen <williamkerpen@hotmail.com>
2026-06-10 23:09:47 +0200

    correcao login

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,19 @@
 const formLogin = document.getElementById('form-login');
 const btnCadastrar = document.getElementById('btn-cadastrar');
 
+
+//
 formLogin.addEventListener('submit', async (event) => {
   event.preventDefault();
 
-  const nome = document.getElementById('nome').value.trim();
+  const email = document.getElementById('email').value.trim();
   const senha = document.getElementById('senha').value.trim();
 
   try {
     const response = await fetch('https://api.fecaf-flix-api.xyz/v1/fecaf-flix/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ nome, senha })
+      body: JSON.stringify({ email, senha })
     });
 
     if (!response.ok) {
@@ -21,7 +23,7 @@ formLogin.addEventListener('submit', async (event) => {
 
     const data = await response.json();
     localStorage.setItem('token', data.token);
-    window.location.href = 'index.html';
+    window.location.href = 'fecaf-flix.html';
   } catch (error) {
     alert('Erro ao conectar com o servidor');
     console.error(error);
@@ -29,16 +31,21 @@ formLogin.addEventListener('submit', async (event) => {
 });
 
 btnCadastrar.addEventListener('click', async () => {
+    const email = prompt('Digite seu E-Mail');
   const nome = prompt('Digite seu nome:');
+  const sobrenome = prompt('Digite seu nome:');
+  const data_nascimento = ('Digite sua Data de Nascimento')
   const senha = prompt('Digite sua senha:');
+  
+  
 
-  if (!nome || !senha) return;
+  if (!nome || !senha || !email || !data_nascimento || !sobrenome) return;
 
   try {
     const response = await fetch('https://api.fecaf-flix-api.xyz/v1/fecaf-flix/user', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ nome, senha })
+      body: JSON.stringify({ nome, senha, email, sobrenome, data_nascimento })
     });
 
     if (response.ok) {
